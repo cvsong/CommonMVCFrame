141ac38567ea90d2f8c12083553ae7026b6e3597
cvsong <491175590@qq.com>
2017-09-05 17:43:43 +0800

    初步分包

--- app/src/main/java/com/cvsong/study/commonmvcframe/business/ui/activity/MainActivity.java
+++ app/src/main/java/com/cvsong/study/commonmvcframe/business/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.cvsong.study.commonmvcframe;
+package com.cvsong.study.commonmvcframe.business.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.cvsong.study.commonmvcframe.R;
 
 public class MainActivity extends AppCompatActivity {
 
